007a8ef064664bab09a5807a1c081000de26f17d
Nánási Dániel <nanasidnl@gmail.com>
2020-10-13 00:18:11 +0200

    docker hub credentials

---
+++ docker-hub-credentials.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl create secret docker_registry regcred 
+docker_server="https://index.docker.io/v1/"
+docker_username="nanasidnl"
+docker_password="..."
+docker_email="nanasidnl@gmail.com"
