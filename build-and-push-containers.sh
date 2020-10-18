f21e55affba9eccf07875addde364655dc42d841
Nánási Dániel <nanasidnl@gmail.com>
2020-10-18 15:09:12 +0200

    optimize disk at deploying

--- build-and-push-containers.sh
+++ build-and-push-containers.sh
@@ -15,15 +15,23 @@ echo "LOGGING IN INTO DOCKER"
 docker login
 
 echo
-echo "BUILD CONTAINERS"
+echo "BUILD PUSH AND PURGE CONTAINERS"
+echo "roscore"
 docker build ${drone_control_dir}/vke_roscore/. -t nanasidnl/drone_control:roscore
-docker build ${drone_control_dir}/vke_aruco/. -t nanasidnl/drone_control:aruco
-docker build ${drone_control_dir}/vke_commander/. -t nanasidnl/drone_control:commander
-docker build ${drone_control_dir}/vke_px4sim/. -t nanasidnl/drone_control:px4sim
-
-echo
-echo "PUSH CONTAINERS"
 docker push nanasidnl/drone_control:roscore
+docker image rm -f $(docker image ls -q)
+
+echo "aruco"
+docker build ${drone_control_dir}/vke_aruco/. -t nanasidnl/drone_control:aruco
 docker push nanasidnl/drone_control:aruco
+docker image rm -f $(docker image ls -q)
+
+echo "commander"
+docker build ${drone_control_dir}/vke_commander/. -t nanasidnl/drone_control:commander
 docker push nanasidnl/drone_control:commander
+docker image rm -f $(docker image ls -q)
+
+echo "px4sim"
+docker build ${drone_control_dir}/vke_px4sim/. -t nanasidnl/drone_control:px4sim
 docker push nanasidnl/drone_control:px4sim
+docker image rm -f $(docker image ls -q)
